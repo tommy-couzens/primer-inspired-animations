c89acd2b856c5e5779ff6dcb7e8e531397634eda
martyn <martyn@automationlogic.com>
2019-07-23 14:59:37 +0100

    added replication, not really working as it should

--- how-life-grows-exponentially/state.js
+++ how-life-grows-exponentially/state.js
@@ -5,16 +5,36 @@ State.epoch = 0
 
 State.drawEpoch = function() {
     ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
     ctx.fillText("Epoch: " + State.epoch, canvas.width - 250, 50);
+    ctx.fillText("Creatures: " + State.creatures.length, canvas.width - 250, 100);
 }
 
 State.nextEpoch = function () {
     this.epoch += 1;
-    this.newCreature();
-}
-
-State.newCreature = function () {
     let x = 25 + Math.random()*(canvas.width - 50);
     let y = 25 + Math.random()*(canvas.height - 50);
-    State.creatures.push(new Creature(x,y));
+    this.newCreature(x, y);
+    this.killCreatures();
+}
+
+State.newCreature = function (x, y ) {
+    State.creatures.push(new blueCreature(x, y));
+}
+
+State.replicateCreatures = function() {
+    state.creatures.forEach(creature => {
+        if (Math.random() < creature.replicationRate) {
+            creature.replicate();
+        }
+    });
+}
+
+State.killCreatures = function() {
+    for (let i = 0; i < this.creatures.length; i++) {
+        creature = this.creatures[i];
+        if (Math.random() < creature.deathRate) {
+            this.creatures.splice(i,1);
+        }
+    }
 }
