b2fbbaf3e64680bb6501478f3c72e8c27bbb17d8
AicyDC <tom.czns@gmail.com>
2019-08-01 11:25:27 +0400

    refactored code into dictionarys of arrays for better code and to redo graphing

--- first-replicators/state.js
+++ first-replicators/state.js
@@ -1,17 +1,28 @@
 let State = {}
 
-State.creatures = []
-State.creatures.blue = []
-State.creatures.green = []
-State.creatures.red = []
-// State.creatures = [State.creatures.blue, State.creatures.green, State.creatures.red]
+State.creatures = {
+    blue: [],
+    green: [],
+    red: []
+}
+
+State.runningTotals = {
+    blue: [0],
+    green: [0],
+    red: [0]
+}
+
 State.epoch = 0
+
+for (const [key, value] of Object.entries(State.runningTotals)) {
+    console.log(key, value);
+}
 
 State.drawEpoch = function() {
     ctx.font = "30px Arial";
     ctx.fillStyle = "black";
     ctx.fillText("Epoch: " + State.epoch, map.width - 250, 50);
-    ctx.fillText("Creatures: " + State.creatures.blue.length, map.width - 250, 100);
+    ctx.fillText("Creatures: " + State.creatures["blue"].length, map.width - 250, 100);
 }
 
 State.nextEpoch = function () {
@@ -20,46 +31,51 @@ State.nextEpoch = function () {
     let y = 25 + Math.random()*(map.height - 50);
     
     this.newBlueCreature(x, y);
-    this.replicateCreatures();
+    // this.replicateCreatures();
     this.mutateCreatures();
     this.killCreatures();
 
-    graph.drawLine(State.creatures.blue, "blue");
-    graph.drawLine(State.creatures.green, "green");
-    graph.drawLine(State.creatures.green, "red");
+    State.runningTotals["blue"].push(State.creatures["blue"].length)
+    State.runningTotals["green"].push(State.creatures["green"].length)
+    State.runningTotals["red"].push(State.creatures["red"].length)
+
+    // for (const [key, value] of Object.entries(State.runningTotals)) {
+    //     State.runningTotals[key].push(State.creatures[key].length)
+    // }
+    // console.log(State.runningTotals['blue'])
+
+
+    // graph.drawLine(State.creatures["blue"], "blue");
+    // graph.drawLine(State.creatures["green"], "green");
+    // graph.drawLine(State.creatures["red"], "red");
 }
 
 State.newBlueCreature = function (x, y, vx, vy, size ) {
-    State.creatures.blue.push(new blueCreature(x, y, vx, vy, size));
+    State.creatures["blue"].push(new blueCreature(x, y, vx, vy, size));
 }
 
 State.newGreenCreature = function (x, y, vx, vy, size ) {
-    State.creatures.green.push(new greenCreature(x, y, vx, vy, size));
+    State.creatures["green"].push(new greenCreature(x, y, vx, vy, size));
 }
 State.newRedCreature = function (x, y, vx, vy, size ) {
-    State.creatures.red.push(new RedCreature(x, y, vx, vy, size));
+    State.creatures["red"].push(new RedCreature(x, y, vx, vy, size));
 }
 
 State.newBlueCreature(100,100);
 
 State.replicateCreatures = function() {
-    State.creatures.forEach(array => {
-        array.forEach(creature => {
+    for (const [key, value] of Object.entries(State.creatures)) {
+        value.forEach(creature => {
             if (Math.random() < creature.replicationRate) {
                 creature.replicate();
             }
         })
-    })
+    }
 
-    // State.creatures.blue.forEach(creature => {
-    //     if (Math.random() < creature.replicationRate) {
-    //         creature.replicate();
-    //     }
-    // });
 }
 
 State.mutateCreatures = function() {
-    State.creatures.blue.forEach(creature => {
+    State.creatures["blue"].forEach(creature => {
         if (Math.random() < creature.mutationRate){
             creature.mutate();
         } else if (Math.random() < creature.redMutationRate) {
