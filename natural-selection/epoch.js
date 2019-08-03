09e1645300934e79d67f64ad20a4cb40a925a579
AicyDC <tom.czns@gmail.com>
2019-08-04 01:53:52 +0400

    going mental with replicants

--- natural-selection/epoch.js
+++ natural-selection/epoch.js
@@ -13,13 +13,30 @@ class Epoch {
     }
 }
 
+const createReplicants = (replicantWorthy) => {
+    let replicants = []
+    replicantWorthy.forEach(creature => {
+        replicants.push(new Creature(creature.position, creature.velocity.mul(-1), creature.speed, creature.size, creature.sense))
+    })
+    return replicants
+}
+
 function nextEpoch(epoch, epochArray) {
-    let worthyCreatures = epoch.creatures.filter(creature => creature.foodEaten > 0)
+    let worthyCreatures = epoch.creatures.filter(creature => creature.foodEaten >= 1)
+    let replicantWorthy = epoch.creatures.filter(creature => creature.foodEaten >= 2)
     worthyCreatures.forEach(function(creature) {
+        creature.energy = 100
+        creature.foodEaten = 0  
+    })
+    console.log(worthyCreatures)
+    
+    console.log(replicantWorthy)
+    let replicants = createReplicants(replicantWorthy)
+    replicants.forEach(function(creature) {
         creature.energy = 100
         creature.foodEaten = 0
     })
-    console.log(worthyCreatures)
+    console.log(replicants)
 
-    epochArray.push(new Epoch(worthyCreatures, generateFood(100)))
+    epochArray.push(new Epoch(worthyCreatures.concat(replicants), generateFood(100)))
 }
