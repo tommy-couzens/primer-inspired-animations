3f5a287cbf303d5cbf7fea7f092b15dc2b61899d
AicyDC <tom.czns@gmail.com>
2019-08-07 02:52:35 +0400

    working replication

--- natural-selection/epoch.js
+++ natural-selection/epoch.js
@@ -16,27 +16,22 @@ class Epoch {
 const createReplicants = (replicantWorthy) => {
     let replicants = []
     replicantWorthy.forEach(creature => {
-        replicants.push(new Creature(creature.position, creature.velocity.mul(-1), creature.speed, creature.size, creature.sense))
+        replicants.push(new Creature(creature.position.plus(new Vector(creature.size, -creature.size)), creature.velocity.mul(-1), creature.speed, creature.size, creature.sense))
     })
     return replicants
 }
 
 function nextEpoch(epoch, epochArray) {
+    console.log(epoch.creatures)
     let worthyCreatures = epoch.creatures.filter(creature => creature.foodEaten >= 1)
-    let replicantWorthy = epoch.creatures.filter(creature => creature.foodEaten >= 2)
-    worthyCreatures.forEach(function(creature) {
+    let replicants = createReplicants(epoch.creatures.filter(creature => creature.foodEaten >= 2))
+    let nextGen = worthyCreatures.concat(replicants)
+
+    nextGen.forEach(function(creature) {
         creature.energy = 100
         creature.foodEaten = 0  
     })
-    console.log(worthyCreatures)
-    
-    console.log(replicantWorthy)
-    let replicants = createReplicants(replicantWorthy)
-    replicants.forEach(function(creature) {
-        creature.energy = 100
-        creature.foodEaten = 0
-    })
-    console.log(replicants)
 
-    epochArray.push(new Epoch(worthyCreatures.concat(replicants), generateFood(100)))
+    epochArray.push(new Epoch(nextGen, generateFood(100)))
+    // epochArray.push(new Epoch(worthyCreatures, generateFood(100)))
 }
