a960230b512b9035f4587896600bb07160b84f4f
AicyDC <tom.czns@gmail.com>
2019-08-25 20:31:28 +0100

    added condition for graph

--- natural-selection/chart.js
+++ natural-selection/chart.js
@@ -10,6 +10,10 @@ speedCanvas.height = window.innerHeight/2 -50
 
 speedCanvas.style.border = "0px solid";
 
+// const drawGraph = false
+const drawGraph = true
+
+
 // Helper function to get creature data into the correct format
 const countMap = (array) => {
 	return new Map([...new Set(array)].map(x => [x, array.filter(y => y ==x).length]).sort())
@@ -29,42 +33,43 @@ function updateGraph(creatures) {
 	senseChart.data.datasets[0].data = Array.from(senseMap.values())
 	senseChart.update()
 }
-
-const speedData1 = {
-    labels: ["1.00"],
-    datasets: [{
-        label: 'Speed',
-        backgroundColor: 'rgb(255, 255, 132)',
-        borderColor: 'rgb(0, 0, 0)',
-        data: [8]
-    }]
-}
-const speedChart = new Chart(speedtx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: speedData1,
-
-    // Configuration options go here
-    options: {
-        layout: {
-            padding: {
-                left: canvas.width + 50,
-                right: 50,
-                top: 50,
-                bottom: canvas.height/2,
-            }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+if (drawGraph) {
+    var speedData1 = {
+        labels: ["1.00"],
+        datasets: [{
+            label: 'Speed',
+            backgroundColor: 'rgb(255, 255, 132)',
+            borderColor: 'rgb(0, 0, 0)',
+            data: [8]
+        }]
     }
-}); 
+    var speedChart = new Chart(speedtx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: speedData1,
+
+        // Configuration options go here
+        options: {
+            layout: {
+                padding: {
+                    left: canvas.width + 50,
+                    right: 50,
+                    top: 50,
+                    bottom: canvas.height/2,
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    }); 
+}
 
 // Define canvas object
 var senseCanvas = document.getElementById("senseChart");
@@ -76,38 +81,40 @@ senseCanvas.height = window.innerHeight/2 -50
 
 senseCanvas.style.border = "0px solid";
 
-const senseData1 = {
-    labels: ["1.00"],
-    datasets: [{
-        label: 'sense',
-        backgroundColor: 'rgb(0, 0, 132)',
-        borderColor: 'rgb(0, 0, 0)',
-        data: [8]
-    }]
-}
-const senseChart = new Chart(sensetx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: senseData1,
-
-    // Configuration options go here
-    options: {
-        layout: {
-            padding: {
-                left: canvas.width + 50,
-                right: 50,
-                top: canvas.height/2,
-                bottom: 50,
-            }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+if (drawGraph) {
+    var senseData1 = {
+        labels: ["1.00"],
+        datasets: [{
+            label: 'sense',
+            backgroundColor: 'rgb(0, 0, 132)',
+            borderColor: 'rgb(0, 0, 0)',
+            data: [8]
+        }]
     }
-}); 
+    var senseChart = new Chart(sensetx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: senseData1,
+
+        // Configuration options go here
+        options: {
+            layout: {
+                padding: {
+                    left: canvas.width + 50,
+                    right: 50,
+                    top: canvas.height/2,
+                    bottom: 50,
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    }); 
+}
